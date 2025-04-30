36853ca2a35042b430e9cdb24cea3df275725764
Nick Phillips <nick@nphillips.com>
2025-04-30 11:07:00 -0400

    Re-enabling Fjalla One font

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,8 +1,14 @@
 import type { Metadata } from "next";
-// import { Fjalla_One } from "next/font/google";
+import { Fjalla_One } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import AppLayout from "@/components/AppLayout/AppLayout";
+
+const fjallaOne = Fjalla_One({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "WatchLikeMe",
@@ -17,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
-      <body>
+      <body className={fjallaOne.className}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
